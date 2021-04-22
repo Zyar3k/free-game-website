15387543e674fe439a07dc4af65f264207e6f8f9
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-22 15:29:01 +0200

    Implement onChange event

--- src/components/GameList/GameList.container.tsx
+++ src/components/GameList/GameList.container.tsx
@@ -1,10 +1,21 @@
-import React, { ReactElement, useState, useEffect } from 'react'
+import React, {
+	ReactElement,
+	useState,
+	useEffect,
+	useCallback,
+	ChangeEvent,
+} from 'react'
 import axios from 'axios'
 import { Game } from 'types'
 import GameListRender from './GameList.render'
 import { API_HOST, API_KEY } from '../../constants'
+import { Filter } from './types'
 
 const GameList = (): ReactElement => {
+	const [filter, setFilter] = useState<Filter>({
+		platform: 'browser',
+		sortBy: 'relevance',
+	})
 	const [games, setGames] = useState<Game[]>([])
 	const [err, setErr] = useState<string>('')
 
@@ -24,7 +35,17 @@ const GameList = (): ReactElement => {
 			.catch(e => setErr(e.message))
 	}, [])
 
-	return <GameListRender err={err} games={games} />
+	const onFilterChange = useCallback((e: ChangeEvent<HTMLFormElement>) => {
+		setFilter(current => ({
+			...current,
+			[e.target.name]: e.target.value,
+		}))
+		e.preventDefault()
+	}, [])
+
+	return (
+		<GameListRender err={err} games={games} onFilterChange={onFilterChange} />
+	)
 }
 
 export default GameList
